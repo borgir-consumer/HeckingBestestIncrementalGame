d0e23f03f11d4c51486a78c25269e6d92001e584
EstDev0 <64747495+borgir-consumer@users.noreply.github.com>
2021-07-13 22:13:53 +0300

    HECKING BESTEST ICREMETAL GEME

---
+++ game.js
@@ -0,0 +1,7 @@
+var nombir = 0
+
+function GivNombir() {
+	nombir += 1
+	document.getElementById("nombirtext").innerHTML = "yur'oe nombir is " + nombir.toLocaleString() + ". FOOKIN' " + nombir.toLocaleString() + " INNIT BRUV!!!!!!!!!!!!!!!!111111"
+}
+	
