e9d7ef940be6f8e8e5ceb014f167e59c460e7d77
EstDev0 <64747495+borgir-consumer@users.noreply.github.com>
2021-07-14 16:49:14 +0300

    HECKING BESTEST ICREMETAL GEME

--- game.js
+++ game.js
@@ -2,8 +2,9 @@ var nombir = 0
 var audio = new Audio("OhBabyATriple.mp3")
 
 function GivNombir() {
+	audio.pause()
+	audio.currentTime = 0;
 	audio.play()
 	nombir += 1
 	document.getElementById("nombirtext").innerHTML = "yur'oe nombir is " + nombir.toLocaleString() + ". FOOKIN' " + nombir.toLocaleString() + " INNIT BRUV!!!!!!!!!!!!!!!!111111"
 }
-	
